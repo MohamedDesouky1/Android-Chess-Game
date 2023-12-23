6be26cb76622d152a330a52a9e51f6535bbb5e70
Mohamed Desouki <kemo.desoki@gmail.com>
2023-12-24 02:03:14 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import GameScreen from './screens/GameScreen';
+import ResultScreen from './screens/ResultScreen';
+
+
+const Stack = createStackNavigator();
+
+
+function App () {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="GameScreen" component={GameScreen} />
+        <Stack.Screen name="ResultScreen" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
