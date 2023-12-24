8d7be6330b03047be34f4e871e4c66255a350e4f
Mohamed Desouki <kemo.desoki@gmail.com>
2023-12-24 15:20:56 +0400

    Update ChessBoard.js

--- components/ChessBoard.js
+++ components/ChessBoard.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import ChessPiece from './ChessPiece';
 
-const ChessBoard = () => {
+const ChessBoard = ({ board, handleMove }) => {
   return (
     <View>
-      <Text>Chess Board Component</Text>
-      {}
+      {board.map((row, rowIndex) => (
+        <View key={rowIndex} style={{ flexDirection: 'row' }}>
+          {row.map((piece, colIndex) => (
+            <View
+              key={colIndex}
+              onTouchEnd={() => handleMove(0, 0, rowIndex, colIndex)}
+              style={{
+                width: 40,
+                height: 40,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <ChessPiece piece={piece} />
+            </View>
+          ))}
+        </View>
+      ))}
     </View>
   );
 };
